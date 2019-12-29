3527aea20594ca0694281e25a7608df8b3f56872
EwelinaBamberska <ewelinaba@98wp.pl>
2019-12-29 22:44:41 +0100

    server

--- new_server.cpp
+++ new_server.cpp
@@ -21,7 +21,7 @@ fd_set rmask;
 int fdmax, fda, rc;
 char buf [100];
 struct timeval timeout;
-bool if_data_actually_read = false;
+bool if_data_actually_read = false, last_line = false;
 
 
 size_t my_itoa( char *s, unsigned int n )
@@ -128,63 +128,57 @@ int main(int argc, char *argv[]) {
 				}
 				
 				//read amount of data
-				else if(get_state_of_client(i) == 1 && client_fd != i && !if_data_actually_read){
+				else if(get_state_of_client(i) == 1 && client_fd != i && !if_data_actually_read && !last_line){
 					if_data_actually_read = true;
 					actual_client = i;
 					bufff = new char[12];
 				 	read(i, bufff, 12);
 				 	int  j = 0;
 					signs_count = atoi(bufff);
-//					printf("%d\n", signs_count);
+
 					set_state_of_client(i, 4);
 					FD_SET(client_fd, &wmask);
 					FD_CLR(i, &rmask);
 				}
 
-					//write(client_fd, "\n", 2);
-//
-//					if(j == 0){
-//					printf("%s Count: %d\n", buffer, signs_count);
-//						j ++;
-//					}
-//
                 else if(get_state_of_client(i) == 4 && client_fd != i && actual_client == i){
 					bufff = new char[5000];
-//					while(1){
+
                     one_time_len = read(i, bufff, 5000);
                     actual_signs_count += one_time_len;
                     if(actual_signs_count != 0)
                     printf(" %d         %d\n", actual_signs_count, signs_count);
                     if(actual_signs_count >= signs_count){
-                        printf("KONIEC\n");
-                        set_state_of_client(client_fd, 5);
 
-                        set_state_of_client(client_fd, 1);
+                        set_state_of_client(i, 5);
+		
                         actual_client = -1;
+						last_line = true;
+						
                         if_data_actually_read = false;
                         signs_count = 0;
                         actual_signs_count = 0;
-                        break;
+                        
+                    	FD_SET(client_fd, &wmask);
                         close(i);
+                        break;
                     }
                     FD_CLR(i, &rmask);
                     FD_SET(client_fd, &wmask);
-//					}
+
 				}
-				
-//				FD_CLR(i, &rmask);
-				//FD_SET(i, &wmask);
-				//printf("%s\n", bufff);
 			}
 		}
 		if(FD_ISSET(client_fd, &wmask)) {
-		    if(if_data_actually_read) {
+		    if(if_data_actually_read || last_line) {
+		    	if(last_line)
+		    		printf("KSDFSGER\n");
                 if (get_state_of_client(client_fd) == 1) {
                     write(client_fd, bufff, 12);
                     char converted_ident[10];
                     int s = my_itoa(converted_ident, actual_client);
                     converted_ident[9] = '\n';
-//                    printf("size %d %s\n", actual_client, converted_ident);
+
                     write(client_fd, converted_ident, 10);
                     set_state_of_client(client_fd, 4);
                     FD_CLR(client_fd, &wmask);
@@ -193,16 +187,16 @@ int main(int argc, char *argv[]) {
                 //printf("write\n");
                 if (get_state_of_client(client_fd) == 4) {
                     write(client_fd, bufff, one_time_len);
+                    last_line = false;
                     FD_CLR(client_fd, &wmask);
-                    FD_SET(actual_client, &rmask);
+                    if(!last_line)
+                    	FD_SET(actual_client, &rmask);
+                    if(last_line){
+                    	set_state_of_client(client_fd, 1);
+                    	printf("END\n");
+                    }
                 }
             }
-//
-//				FD_CLR(i, &wmask);
-//				if(i == fdmax)
-//					while(fdmax > fd && ! FD_ISSET(fdmax, &mask))
-//						fdmax -= 1;
-//				printf("Closed\n");
 			}
     	
     }
